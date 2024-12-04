8a3b106afd88561dc0adb016e48d8057c58f7837
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-12-04 11:21:13 -0300

    created views and urls for get and post functions

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from . import views
 
 app_name = 'recipes_app'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('recipes/', views.list_recipes, name='list_recipes'),
+    path('recipes/<int:id>/', views.getRecipeByID, name='recipe_by_id'),
+    path('recipe/', views.createRecipe, name='create_recipe')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import  JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
 
 import json
+
 
 def home(request):
     data = {
@@ -10,4 +21,29 @@ def home(request):
     }
 
     return JsonResponse(data)
+
+
+def list_recipes(request):
+    recipes = Recipe.objects.all()
+    recipes_list = list(recipes.values())
+    return JsonResponse(recipes_list, safe=False)
+
+
+@api_view(['GET'])
+def getRecipeByID(request, id):
+    try:
+        recipe = Recipe.objects.get(id=id)
+        serializer = RecipeSerializer(recipe)
+        return Response(serializer.data)
+    except Recipe.DoesNotExist:
+        return Response('This recipe does not exist')
+    
+    
+@api_view(['POST'])
+def createRecipe(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
